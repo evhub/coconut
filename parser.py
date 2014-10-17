ccfa27ed05610a2006b44c6179c72218acdf816d
evhub <evanjhub@gmail.com>
2014-10-16 18:58:33 -0700

    Fixes lots and lots of parsing errors

--- parser.py
+++ parser.py
@@ -36,6 +36,8 @@ white = " \t\f"
 # GRAMMAR:
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+refs = None
+
 class pre(object):
     """The CoconutScript Pre-Processor."""
     downs="([{"
@@ -50,6 +52,8 @@ class pre(object):
     def __init__(self):
         """Creates A New Pre-Processor."""
         self.refs = []
+        global refs
+        refs = self.refs
 
     def proc(self, inputstring):
         """Performs Pre-Processing."""
@@ -90,7 +94,7 @@ class pre(object):
                     elif c == hold[1][0]:
                         hold[2] += c
                     elif len(hold[2]) > len(hold[1]):
-                        raise ParseException("Invalid number of string closes in char #"+x)
+                        raise ParseFatalException("Invalid number of string closes in char #"+x)
                     elif hold[2] == hold[1]:
                         out.append(self.wrapstr(hold[0], hold[1][0] in self.raw, True))
                         hold = None
@@ -121,7 +125,7 @@ class pre(object):
                     hold = [c, found, None]
                     found = None
                 else:
-                    raise ParseException("Invalid number of string starts in char #"+x)
+                    raise ParseFatalException("Invalid number of string starts in char #"+x)
             elif c in self.comment:
                 hold = [""]
             elif c in self.holds:
@@ -130,7 +134,7 @@ class pre(object):
                 out.append(c)
             x += 1
         if hold is not None or found is not None:
-            raise ParseException("Unclosed string in char #"+x)
+            raise ParseFatalException("Unclosed string in char #"+x)
         return "".join(out)
 
     def leading(self, inputstring):
@@ -142,7 +146,7 @@ class pre(object):
             elif self.indchar is None:
                 self.indchar = c
             elif self.indchar != c:
-                raise ParseException("Illegal mixing of tabs and spaces in line "+inputstring)
+                raise ParseFatalException("Illegal mixing of tabs and spaces in line "+inputstring)
             count += 1
         return count
 
@@ -172,14 +176,14 @@ class pre(object):
         count = 0
         for x in xrange(0, len(lines)):
             if lines[x] and lines[x][-1] in white:
-                raise ParseException("Illegal trailing whitespace in line "+lines[x]+" (#"+str(x)+")")
+                raise ParseFatalException("Illegal trailing whitespace in line "+lines[x]+" (#"+str(x)+")")
             elif count < 0:
                 new[-1] += lines[x]
             else:
                 check = self.leading(lines[x])
                 if not x:
                     if check:
-                        raise ParseException("Illegal initial indent in line "+lines[x]+" (#"+str(x)+")")
+                        raise ParseFatalException("Illegal initial indent in line "+lines[x]+" (#"+str(x)+")")
                     else:
                         current = 0
                 elif check > current:
@@ -188,18 +192,21 @@ class pre(object):
                     lines[x] = openstr+lines[x]
                 elif check in levels:
                     point = levels.index(check)+1
-                    new[-1] += closestr*(len(levels[point:])+1)
+                    lines[x] = closestr*(len(levels[point:])+1)+lines[x]
                     levels = levels[:point]
                     current = levels.pop()
                 elif current != check:
-                    raise ParseException("Illegal dedent to unused indentation level in line "+lines[x]+" (#"+str(x)+")")
+                    raise ParseFatalException("Illegal dedent to unused indentation level in line "+lines[x]+" (#"+str(x)+")")
                 new.append(lines[x])
             count += self.change(lines[x])
-        if new:
-            new[-1] += closestr*(len(levels)-1)
+        new.append(closestr*(len(levels)-1))
         return linebreak.join(new)
 
 ParserElement.setDefaultWhitespaceChars(white)
+
+def fixto(item, output):
+    """Forces An Item To Result In A Specific Output."""
+    return item.setParseAction(replaceWith(output))
 
 comma = Literal(",")
 dot = Literal(".")
@@ -208,8 +215,8 @@ dubstar = Literal("**")
 lparen = Literal("(")
 rparen = Literal(")")
 at = Literal("@")
-arrow = Literal("->") | Literal("\u2192")
-heavy_arrow = Literal("=>") | Literal("\u21d2")
+arrow = fixto(Literal("->") | Literal("\u2192"), "->")
+heavy_arrow = fixto(Literal("=>") | Literal("\u21d2"), "=>")
 colon = Literal(":")
 semicolon = Literal(";")
 equals = Literal("=")
@@ -219,30 +226,30 @@ lbrace = Literal("{")
 rbrace = Literal("}")
 plus = Literal("+")
 minus = Literal("-")
-bang = Literal("!") | Literal("\xac")
+bang = fixto(Literal("!") | Literal("\xac"), "!")
 slash = Literal("/")
-dubslash = Literal("//") | Literal("\u20eb")
-pipeline = Literal("|>") | Literal("\u21a6")
-amp = Literal("&") | Literal("\u2227") | Literal("\u2229")
-caret = Literal("^") | Literal("\u22bb") | Literal("\u2295")
-bar = Literal("|") | Literal("\u2228") | Literal("\u222a")
+dubslash = fixto(Literal("//") | Literal("\u20eb"), "//")
+pipeline = fixto(Literal("|>") | Literal("\u21a6"), "|>")
+amp = fixto(Literal("&") | Literal("\u2227") | Literal("\u2229"), "&")
+caret = fixto(Literal("^") | Literal("\u22bb") | Literal("\u2295"), "^")
+bar = fixto(Literal("|") | Literal("\u2228") | Literal("\u222a"), "|")
 percent = Literal("%")
 dotdot = Literal("..")
 dollar = Literal("$")
-ellipses = Literal("...") | Literal("\u2026")
-lshift = Literal("<<") | Literal("\xab")
-rshift = Literal(">>") | Literal("\xbb")
+ellipses = fixto(Literal("...") | Literal("\u2026"), "...")
+lshift = fixto(Literal("<<") | Literal("\xab"), "<<")
+rshift = fixto(Literal(">>") | Literal("\xbb"), ">>")
 tilde = Literal("~")
 underscore = Literal("_")
 pound = Literal("#")
 backslash = Literal("\\")
 
-mul_star = star | Literal("\xd7")
-exp_dubstar = star | Literal("\xd7\xd7")
-neg_minus = minus | Literal("\xaf")
-sub_minus = minus | Literal("\u2212")
-div_slash = slash | Literal("\xf7")
-div_dubslash = dubslash | Combine(Literal("\xf7"), slash)
+mul_star = fixto(star | Literal("\xd7"), "*")
+exp_dubstar = fixto(star | Literal("\xd7\xd7"), "**")
+neg_minus = fixto(minus | Literal("\xaf"), "-")
+sub_minus = fixto(minus | Literal("\u2212"), "-")
+div_slash = fixto(slash | Literal("\xf7"), "/")
+div_dubslash = fixto(dubslash | Combine(Literal("\xf7"), slash), "//")
 mod_percent = percent
 
 NAME = Regex(r"(?![0-9])\w")
@@ -254,62 +261,106 @@ octint = Word("01234567")
 hexint = Word(hexnums)
 anyint = Word(nums, alphanums)
 
-basenum = integer | Combine(integer + dot + Optional(integer))
-sci_e = CaselessLiteral("e") | Literal("\u23e8")
-numitem = basenum | Combine(basenum + sci_e + integer)
+basenum = Combine(integer + dot + Optional(integer)) | integer
+sci_e = CaselessLiteral("e") | fixto(Literal("\u23e8"), "E")
+numitem = Combine(basenum + sci_e + integer) | basenum
 
-NUMBER = (numitem
+def anyint_proc(tokens):
+    """Replaces Underscored Integers."""
+    if len(tokens) == 1:
+        item, base = tokens[0].split("_")
+        tokens[0] = 'int("'+item+'", '+base+")"
+        return tokens
+    else:
+        raise ParseFatalException("Invalid anyint token")
+
+NUMBER = (Combine(anyint + underscore + integer).setParseAction(anyint_proc)
           | Combine(CaselessLiteral("0b"), binint)
           | Combine(CaselessLiteral("0o"), octint)
           | Combine(CaselessLiteral("0x"), hexint)
-          | Combine(anyint + underscore + integer)
+          | numitem
           )
 
+def string_repl(tokens):
+    """Replaces String References."""
+    if len(tokens) == 1:
+        tokens[0] = refs[int(tokens[0])]
+        if isinstance(tokens[0], tuple):
+            tokens[0], raw, multiline = tokens[0]
+            if tokens[0]:
+                if tokens[0][-1] == '"':
+                    tokens[0] = tokens[:-1]+'\\"'
+                if tokens[0][0] == '"':
+                    tokens[0] = "\\"+tokens[0]
+            if multiline:
+                tokens[0] = '"""'+tokens[0]+'"""'
+            else:
+                tokens[0] = '"'+tokens[0]+'"'
+            if raw:
+                tokens[0] = "r"+tokens[0]
+            return tokens
+        else:
+            raise ParseFatalException("String marker points to comment")
+    else:
+        raise ParseFatalException("Invalid string marker")
+
+def comment_repl(tokens):
+    """Replaces Comment References."""
+    if len(tokens) == 1:
+        tokens[0] = refs[int(tokens[0])]
+        if isinstance(tokens[0], tuple):
+            raise ParseFatalException("Comment marker points to string")
+        else:
+            return tokens
+    else:
+        raise ParseFatalException("Invalid comment marker")
+
+string_ref = Combine(Literal('"').suppress() + integer + Literal('"').suppress()).setParseAction(string_repl)
+comment = Combine(pound.suppress() + integer).setParseAction(comment_repl)
 bit_b = CaselessLiteral("b")
-STRING = Combine(Optional(bit_b) + Literal('"') + integer + Literal('"'))
-comment = Combine(pound + integer)
+STRING = Combine(Optional(bit_b) + string_ref)
 NEWLINE = Optional(comment) + Literal(linebreak)
 STARTMARKER = Literal(start).suppress()
 ENDMARKER = Literal(end).suppress()
 INDENT = Literal(openstr)
 DEDENT = Literal(closestr) + Optional(NEWLINE)
 
-augassign = (Combine(plus + equals)
+augassign = (heavy_arrow # In-place pipeline
+             | Combine(plus + equals)
              | Combine(sub_minus + equals)
              | Combine(mul_star + equals)
              | Combine(exp_dubstar + equals)
              | Combine(div_slash + equals)
              | Combine(mod_percent + equals)
              | Combine(amp + equals)
-             | combine(bar + equals)
+             | Combine(bar + equals)
              | Combine(caret + equals)
              | Combine(lshift + equals)
              | Combine(rshift + equals)
-             | combine(div_dubslash + equals)
+             | Combine(div_dubslash + equals)
              | Combine(OneOrMore(tilde) + equals)
              | Combine(dotdot + equals)
-             | heavy_arrow # In-place pipeline
              )
 
 comp_op = (Literal("<")
            | Literal(">")
            | Literal("==")
-           | (
+           | fixto(
                Literal(">=")
-               | Literal("\u2265")
+               | Literal("\u2265"), ">="
                )
-           | (
+           | fixto(
                Literal("<=")
-               | Literal("\u2264")
+               | Literal("\u2264"), "<="
                )
-           | (
+           | fixto(
                Combine(bang, equals)
-               | Literal("\u2260")
+               | Literal("\u2260"), "!="
                )
-           | Keyword("in")
            | Keyword("not") + Keyword("in")
-           | Keyword("is")
+           | Keyword("in")
            | Keyword("is") + Keyword("not")
+           | Keyword("is")
            )
 
 test = Forward()
@@ -340,22 +391,22 @@ func_atom = NAME | lparen + Optional(yield_expr | testlist_comp) + rparen
 atom = (func_atom
         | lbrack + Optional(testlist_comp) + rbrack
         | lbrace + Optional(dictorsetmaker) + rbrace
-        | NUMBER
-        | OneOrMore(STRING)
         | ellipses
         | Keyword("None")
         | Keyword("True")
         | Keyword("False")
+        | NUMBER
+        | OneOrMore(STRING)
         )
 sliceop = colon + Optional(test)
 subscript = test | Optional(test) + colon + Optional(test) + Optional(sliceop)
 subscriptlist = subscript + ZeroOrMore(comma + subscript) + Optional(comma).suppress()
-trailer = Optional(dollar) + lparen + Optional(argslist) + rparen | lbrack + subscriptlist + rbrack | dot + NAME | dotdot + func_atom
+trailer = Optional(dollar) + lparen + Optional(argslist) + rparen | lbrack + subscriptlist + rbrack | dotdot + func_atom | dot + NAME
 item = atom + ZeroOrMore(trailer)
 factor = Forward()
 power = item + Optional(exp_dubstar + factor)
 unary = plus | neg_minus | bang
-factor <<= power | unary + factor
+factor <<= unary + factor | power
 mulop = mul_star | div_slash | div_dubslash | mod_percent
 term = factor + ZeroOrMore(mulop + factor)
 arith = plus | sub_minus
@@ -379,14 +430,14 @@ lambdef = parameters + arrow + test
 lambdef_nocond = parameters + arrow + test_nocond
 test <<= test_item + Optional(Keyword("if") + test_item + Keyword("else") + test) | lambdef
 test_nocond <<= test_item | lambdef_nocond
-exprlist = (expr | star_expr) + ZeroOrMore(comma + (expr | star_expr)) + Optional(comma).suppress()
+exprlist = (star_expr | expr) + ZeroOrMore(comma + (star_expr | expr)) + Optional(comma).suppress()
 
 suite = Forward()
 
-argument = NAME + Optional(comp_for) | NAME + equals + test
-arglist = ZeroOrMore(argument + comma) + (argument + Optional(comma)
+argument = NAME + equals + test | NAME + Optional(comp_for)
+arglist = ZeroOrMore(argument + comma) + ( dubstar + test
                                           | star + test + ZeroOrMore(comma + argument) + Optional(comma + dubstar + test)
-                                          | dubstar + test)
+                                          | argument + Optional(comma))
 classdef = Keyword("class") + NAME + Optional(lparen + Optional(arglist) + rparen) + colon + suite
 comp_iter = Forward()
 comp_for <<= Keyword("for") + exprlist + Keyword("in") + test_item + Optional(comp_iter)
@@ -403,7 +454,7 @@ flow_stmt = break_stmt | continue_stmt | return_stmt | raise_stmt | yield_stmt
 
 def parenwrap(item):
     """Wraps An Item In Optional Parentheses."""
-    return item | lparen.suppress() + item + rparen.suppress()
+    return lparen.suppress() + item + rparen.suppress() | item
 
 dotted_as_name = dotted_name + Optional(Keyword("as") + NAME)
 import_as_name = NAME + Optional(Keyword("as") + NAME)
@@ -438,18 +489,22 @@ decorated = decorators + (classdef | funcdef)
 
 compound_stmt = if_stmt | while_stmt | for_stmt | try_stmt | with_stmt | funcdef | classdef | decorated
 expr_stmt = testlist_star_expr + (augassign + (yield_expr | testlist) | ZeroOrMore(equals + (yield_expr | testlist_star_expr)))
-small_stmt = expr_stmt | del_stmt | pass_stmt | flow_stmt | import_stmt | global_stmt | nonlocal_stmt | assert_stmt
+small_stmt = del_stmt | pass_stmt | flow_stmt | import_stmt | global_stmt | nonlocal_stmt | assert_stmt | expr_stmt
 simple_stmt = small_stmt + ZeroOrMore(semicolon + small_stmt) + Optional(semicolon).suppress() + NEWLINE
-stmt = simple_stmt | compound_stmt
-suite <<= simple_stmt | NEWLINE + INDENT + OneOrMore(stmt) + DEDENT
+stmt = compound_stmt | simple_stmt
+suite <<= NEWLINE + INDENT + OneOrMore(stmt) + DEDENT | simple_stmt
 
-single_input = NEWLINE | simple_stmt | compound_stmt + NEWLINE
+single_input = NEWLINE | compound_stmt + NEWLINE | simple_stmt
 file_input = ZeroOrMore(NEWLINE | stmt)
 eval_input = testlist + Optional(comment)
 
-single_parser = STARTMARKER + single_input + ENDMARKER
-file_parser = STARTMARKER + file_input + ENDMARKER
-eval_parser = STARTMARKER + eval_input + ENDMARKER
+single_parser = OneOrMore(STARTMARKER + single_input + ENDMARKER)
+file_parser = OneOrMore(STARTMARKER + file_input + ENDMARKER)
+eval_parser = OneOrMore(STARTMARKER + eval_input + ENDMARKER)
+
+def parsewith(parser, item):
+    """Tests Parsing With A Parser."""
+    return (StringStart() + parser + StringEnd()).parseString(item)
 
 def parse_single(inputstring):
     """Processes Console Input."""
