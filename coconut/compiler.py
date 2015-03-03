d05668fac74b5c5f752fb73679875ff6ae753294
Evan Hubinger <evhub@ripple.com>
2015-03-02 22:51:18 -0800

    Fixes running code from the command line

--- coconut/compiler.py
+++ coconut/compiler.py
@@ -79,7 +79,7 @@ class cli(object):
     commandline.add_argument("-r", "--run", action="store_const", const=True, default=False, help="run the compiled source instead of writing it")
     commandline.add_argument("-i", "--interact", action="store_const", const=True, default=False, help="force the interpreter to start (otherwise starts if no other command is given)")
     commandline.add_argument("-d", "--debug", metavar="level", type=int, nargs="?", default=0, const=1, help="enable debug output (level: 0 is off, no arg defaults to 1, max is "+str(max_debug)+")")
-    commandline.add_argument("-c", "--code", metavar="code", type=str, nargs=1, help="run a line of coconut passed in as a string")
+    commandline.add_argument("-c", "--code", metavar="code", type=str, nargs=1, default=None, help="run a line of coconut passed in as a string")
     commandline.add_argument("--autopep8", type=str, nargs=argparse.REMAINDER, default=None, help="use autopep8 to format compiled code (remaining args passed to autopep8)")
     processor = None
     debug = False
@@ -123,7 +123,7 @@ class cli(object):
         if args.autopep8 is not None:
             self.processor.autopep8(args.autopep8)
         if args.code is not None:
-            self.execute(self.processor.parse_single(args.code))
+            self.execute(self.processor.parse_single(args.code[0]))
         if args.source is not None:
             if args.run:
                 if args.dest is not None:
