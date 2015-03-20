1478c09af46810d490121a73b3c67c83b3b0735d
Evan Hubinger <evhub@ripple.com>
2015-03-20 16:48:47 -0700

    Removes support for Python 2.6

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setuptools.setup(
         "Environment :: Console",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.0",
