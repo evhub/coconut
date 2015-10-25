f0524326b22cf31df8fef6b8eeeeaa3c36ed1184
Evan Hubinger <evanjhub@gmail.com>
2015-10-24 22:27:41 -0700

    Tries creating temporary files

--- coconut/py2_exec.py
+++ coconut/py2_exec.py
@@ -16,11 +16,15 @@ Description: The Python 3 exec function in Python 2.
 
 from __future__ import with_statement, print_function, absolute_import, unicode_literals, division
 
-from StringIO import StringIO
+from tempfile import NamedTemporaryFile
 
 #-----------------------------------------------------------------------------------------------------------------------
 # FUNCTION:
 #-----------------------------------------------------------------------------------------------------------------------
 
 def execfunc(code, gvars=None, lvars=None):
-    return execfile(StringIO(code), gvars, lvars)
+    codefile = NamedTemporaryFile()
+    codefile.write(code)
+    codefilename = codefile.name
+    codefile.close()
+    return execfile(codefilename, gvars, lvars)
