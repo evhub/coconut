dd00b390ace40c43ef44bbaaebcc066b51e457b0
Evan Hubinger <evanjhub@gmail.com>
2015-11-20 16:47:19 -0800

    Further improves str bytes constructors

--- coconut/root.py
+++ coconut/root.py
@@ -56,30 +56,22 @@ py2_chr, chr = chr, unichr
 py2_str = str
 _coconut_str, _coconut_unicode = py2_str, unicode
 _coconut_new_int = int
-class _coconut_metabytes(type):
-    def __instancecheck__(cls, inst):
-        return isinstance(inst, _coconut_str)
-class bytes(_coconut_str):
-    """Python 3 bytes."""
-    __metaclass__ = _coconut_metabytes
-    def __init__(self, *args, **kwargs):
-        """Python 3 bytes constructor."""
-        if len(args) == 1 and isinstance(args[0], _coconut_new_int):
-            _coconut_str.__init__(self, b"\x00" * args[0])
-        else:
-            _coconut_str.__init__(self, *args, **kwargs)
-class _coconut_metastr(type):
-    def __instancecheck__(cls, inst):
-        return isinstance(inst, _coconut_unicode)
-class str(_coconut_unicode):
-    """Python 3 str."""
-    __metaclass__ = _coconut_metastr
-    def __init__(self, *args, **kwargs):
-        """Python 3 str constructor."""
-        if len(args) == 1 and isinstance(args[0], _coconut_str):
-            return _coconut_unicode.__init__(self, repr(args[0]))
-        else:
-            return _coconut_unicode.__init__(self, *args, **kwargs)
+bytes = _coconut_str
+def _coconut_bytes_init(self, *args, **kwargs):
+    """Python 3 bytes constructor."""
+    if len(args) == 1 and isinstance(args[0], _coconut_new_int):
+        _coconut_str.__init__(self, b"\x00" * args[0], **kwargs)
+    else:
+        _coconut_str.__init__(self, *args, **kwargs)
+bytes.__init__ = _coconut_bytes_init
+str = _coconut_unicode
+def _coconut_str_init(self, *args, **kwargs):
+    """Python 3 str constructor."""
+    if len(args) == 1 and isinstance(args[0], _coconut_str):
+        return _coconut_unicode.__init__(self, repr(args[0]), **kwargs)
+    else:
+        return _coconut_unicode.__init__(self, *args, **kwargs)
+str.__init__ = _coconut_str_init
 _coconut_encoding = "'''+ENCODING+r'''"
 py2_print = print
 _coconut_print = py2_print
