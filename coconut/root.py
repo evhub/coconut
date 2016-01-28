4319be2f3b1d783828aca4e794e80454f2854021
Evan Hubinger <evanjhub@gmail.com>
2016-01-27 17:41:21 -0800

    Revert "Makes heavy use of __builtins__"
    This reverts commit 766aa039508eaaf94adae5a49eee88046900ee78.

--- coconut/root.py
+++ coconut/root.py
@@ -38,37 +38,34 @@ VERSION_STR = VERSION + " [" + VERSION_NAME + "]"
 VERSION_TAG = "v" + VERSION_STR
 PY2 = sys.version_info < (3,)
 PY2_HEADER_BASE = r'''
-py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_int, py2_chr, py2_str, py2_print, py2_input = __builtins__.filter, __builtins__.hex, __builtins__.map, __builtins__.oct, __builtins__.zip, __builtins__.open, __builtins__.range, __builtins__.int, __builtins__.chr, __builtins__.str, __builtins__.print, __builtins__.input
-_coconut_int, _coconut_long, _coconut_str, _coconut_bytearray, _coconut_print, _coconut_unicode, _coconut_raw_input = __builtins__.int, __builtins__.long, __builtins__.str, __builtins__.bytearray, __builtins__.print, __builtins__.unicode, __builtins__.raw_input
-__builtins__.range, __builtins__.chr, __builtins__.str = __builtins__.xrange, __builtins__.unichr, __builtins__.unicode
-import future_builtins as _coconut_future_builtins
-__builtins__.ascii, __builtins__.filter, __builtins__.hex, __builtins__.map, __builtins__.oct, __builtins__.zip = _coconut_future_builtins.ascii, _coconut_future_builtins.filter, _coconut_future_builtins.hex, _coconut_future_builtins.map, _coconut_future_builtins.oct, _coconut_future_builtins.zip
-from io import open as _coconut_new_open
+py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_int, py2_chr, py2_str, py2_print, py2_input = filter, hex, map, oct, zip, open, range, int, chr, str, print, input
+_coconut_int, _coconut_long, _coconut_str, _coconut_bytearray, _coconut_print, _coconut_unicode, _coconut_raw_input = int, long, str, bytearray, print, unicode, raw_input
+range, chr, str = xrange, unichr, unicode
+from future_builtins import *
+from io import open
 class _coconut_metaint(type):
     def __instancecheck__(cls, inst):
         return isinstance(inst, (_coconut_int, _coconut_long))
-class _coconut_new_int(_coconut_int):
+class int(_coconut_int):
     """Python 3 int."""
     __metaclass__ = _coconut_metaint
     __slots__ = ()
 class _coconut_metabytes(type):
     def __instancecheck__(cls, inst):
         return isinstance(inst, _coconut_str)
-class _coconut_new_bytes(_coconut_str):
+class bytes(_coconut_str):
     """Python 3 bytes."""
     __metaclass__ = _coconut_metabytes
     __slots__ = ()
     def __new__(cls, *args, **kwargs):
         """Python 3 bytes constructor."""
         return _coconut_str.__new__(cls, _coconut_bytearray(*args, **kwargs))
-def _coconut_new_print(*args, **kwargs):
+def print(*args, **kwargs):
     """Python 3 print."""
     return _coconut_print(*(_coconut_unicode(x).encode(_coconut_sys.stdout.encoding) for x in args), **kwargs)
-def _coconut_new_input(*args, **kwargs):
+def input(*args, **kwargs):
     """Python 3 input."""
-    return _coconut_raw_input(*args, **kwargs).decode(_coconut_sys.stdout.encoding)
-__builtins__.open, __builtins__.int, __builtins__.bytes, __builtins__.print, __builtins__.input = _coconut_new_open, _coconut_new_int, _coconut_new_bytes, _coconut_new_print, _coconut_new_input
-del __builtins__.raw_input, __builtins__.apply, __builtins__.coerce, __builtins__.execfile, __builtins__.file, __builtins__.reload, __builtins__.intern'''
+    return _coconut_raw_input(*args, **kwargs).decode(_coconut_sys.stdout.encoding)'''
 PY2_HEADER = r'import sys as _coconut_sys, os as _coconut_os' + PY2_HEADER_BASE + "\n"
 PY2_HEADER_CHECK = r'''import sys as _coconut_sys
 if _coconut_sys.version_info < (3,):
