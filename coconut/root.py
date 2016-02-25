b757b2ec39fd1a28f8224d4ef67697aeee0d84e0
Evan Hubinger <evanjhub@gmail.com>
2016-02-24 23:32:56 -0800

    Fixes root not in compiled code

--- coconut/root.py
+++ coconut/root.py
@@ -38,7 +38,7 @@ VERSION_STR = VERSION + " [" + VERSION_NAME + "]"
 VERSION_TAG = "v" + VERSION_STR
 PY2 = sys.version_info < (3,)
 PY2_HEADER_BASE = r'''py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_xrange, py2_int, py2_chr, py2_str, py2_print, py2_input, py2_raw_input = filter, hex, map, oct, zip, open, range, xrange, int, chr, str, print, input, raw_input
-_coconut_NameError, _coconut_int, _coconut_long, _coconut_str, _coconut_bytearray, _coconut_print, _coconut_unicode, _coconut_raw_input, _coconut_xrange, _coconut_reversed, _coconut_repr = NameError, int, long, str, bytearray, print, unicode, raw_input, xrange, reversed, repr
+_coconut_NameError, _coconut_int, _coconut_long, _coconut_str, _coconut_unicode, _coconut_bytearray, _coconut_slice, _coconut_reversed, _coconut_isinstance, _coconut_iter, _coconut_len, _coconut_repr, _coconut_print, _coconut_xrange, _coconut_raw_input = NameError, int, long, str, unicode, bytearray, slice, reversed, isinstance, iter, len, repr, print, xrange, raw_input
 chr, str = unichr, unicode
 from future_builtins import *
 from io import open
@@ -48,39 +48,39 @@ class range(object):
     def __init__(self, *args):
         self._xrange = _coconut_xrange(*args)
     def __iter__(self):
-        return __coconut__.iter(self._xrange)
+        return _coconut_iter(self._xrange)
     def __reversed__(self):
         return _coconut_reversed(self._xrange)
     def __len__(self):
-        return __coconut__.len(self._xrange)
+        return _coconut_len(self._xrange)
     def __getitem__(self, index):
-        if __coconut__.isinstance(index, __coconut__.slice):
+        if _coconut_isinstance(index, _coconut_slice):
             start, stop, step = index.start, index.stop, index.step
             if start is None:
                 start = 0
             elif start < 0:
-                start += __coconut__.len(self._xrange)
+                start += _coconut_len(self._xrange)
             if stop is None:
-                stop = __coconut__.len(self._xrange)
+                stop = _coconut_len(self._xrange)
             elif stop is not None and stop < 0:
-                stop += __coconut__.len(self._xrange)
+                stop += _coconut_len(self._xrange)
             if step is None:
                 step = 1
-            return __coconut__.map(self._xrange.__getitem__, __coconut__.range(start, stop, step))
+            return map(self._xrange.__getitem__, range(start, stop, step))
         else:
             return self._xrange[index]
     def __repr__(self):
         return _coconut_repr(self._xrange)[1:]
 class _coconut_metaint(type):
     def __instancecheck__(cls, inst):
-        return __coconut__.isinstance(inst, (_coconut_int, _coconut_long))
+        return _coconut_isinstance(inst, (_coconut_int, _coconut_long))
 class int(_coconut_int):
     __doc__ = _coconut_int.__doc__
     __metaclass__ = _coconut_metaint
     __slots__ = ()
 class _coconut_metabytes(type):
     def __instancecheck__(cls, inst):
-        return __coconut__.isinstance(inst, _coconut_str)
+        return _coconut_isinstance(inst, _coconut_str)
 class bytes(_coconut_str):
     __doc__ = _coconut_str.__doc__
     __metaclass__ = _coconut_metabytes
