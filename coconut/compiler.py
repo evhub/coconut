575044d3c6b1dd14d259f56a3974d882377db0e1
Evan Hubinger <evanjhub@gmail.com>
2016-03-12 23:32:24 -0800

    Fixes handling of raw_input, xrange

--- coconut/compiler.py
+++ coconut/compiler.py
@@ -281,10 +281,10 @@ _coconut_sys.path.insert(0, _coconut_file_path)'''
             if version is None:
                 header += r'''
 if _coconut_sys.version_info < (3,):
-    from __coconut__ import py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_xrange, py2_int, py2_chr, py2_str, py2_print, py2_input, py2_raw_input, ascii, filter, hex, oct, open, range, int, chr, str, print, input, bytes'''
+    from __coconut__ import py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_xrange, py2_int, py2_chr, py2_str, py2_print, py2_input, py2_raw_input, ascii, filter, hex, oct, open, range, int, chr, str, print, input, bytes, raw_input, xrange'''
             elif version == "2":
                 header += r'''
-from __coconut__ import py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_xrange, py2_int, py2_chr, py2_str, py2_print, py2_input, py2_raw_input, ascii, filter, hex, oct, open, range, int, chr, str, print, input, bytes'''
+from __coconut__ import py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_xrange, py2_int, py2_chr, py2_str, py2_print, py2_input, py2_raw_input, ascii, filter, hex, oct, open, range, int, chr, str, print, input, bytes, raw_input, xrange'''
             header += r'''
 from __coconut__ import __coconut__, __coconut_version__, map, zip, reduce, takewhile, dropwhile, tee, count, recursive, datamaker, consume, MatchError
 _coconut_sys.path.remove(_coconut_file_path)
