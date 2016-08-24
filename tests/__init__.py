3291476c7c9814625a88d209f71dc9cd5bebab2c
Fred Buchanan <fred@pineapplecomputerart.com>
2016-08-24 16:34:16 -0400

    Added __init__.py for tests

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Authors: Fred Buchanan
+License: Apache 2.0
+Description: Coconut test suite
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from .intagration_tests import OldCocoTest
