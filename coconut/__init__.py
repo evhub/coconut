19a06b13cfdb5370d4d666171091f1c25984e260
Evan Hubinger <evanjhub@gmail.com>
2016-08-26 11:12:05 -0700

    Fixes handling of icoconut exceptions

--- coconut/__init__.py
+++ coconut/__init__.py
@@ -24,14 +24,19 @@ from coconut.root import *
 
 def load_ipython_extension(ipython):
     """Loads Coconut as an IPython extension."""
-    from coconut.convenience import cmd, parse
+    from coconut.convenience import cmd, parse, CoconutException
+    from coconut.logging import logger
     def magic(line, cell=None):
         """Coconut IPython magic."""
-        if cell is None:
-            code = line
+        try:
+            if cell is None:
+                code = line
+            else:
+                cmd(line) # first line in block is cmd
+                code = cell
+            compiled = parse(code)
+        except CoconutException:
+            logger.print_exc()
         else:
-            cmd(line) # first line in block is cmd
-            code = cell
-        compiled = parse(code)
-        ipython.run_cell(compiled, shell_futures=False)
+            ipython.run_cell(compiled, shell_futures=False)
     ipython.register_magic_function(magic, "line_cell", "coconut")
