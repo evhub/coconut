0830449cc4407f9086b54e1ca2fc1e5b11d752fc
Evan Hubinger <evanjhub@gmail.com>
2016-09-23 13:15:59 -0700

    Increases minimum process time

--- coconut/constants.py
+++ coconut/constants.py
@@ -187,7 +187,7 @@ default_vi_mode = False
 default_mouse_support = True
 
 minimum_sleep_time = .001  # seconds
-minimum_process_time = .01  # seconds
+minimum_process_time = .1  # seconds
 watch_interval = .1  # seconds
 
 info_tabulation = 18  # offset for tabulated info messages
