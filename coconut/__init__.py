6b5e311a86740336596276c4754b1b69ffd808ad
Evan Hubinger <evanjhub@gmail.com>
2016-09-25 18:06:29 -0700

    Moves license boilerplate out of license file

--- coconut/__init__.py
+++ coconut/__init__.py
@@ -6,9 +6,19 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 """
-Author: Evan Hubinger
-License: Apache 2.0
-Description: The Coconut Programming Language.
+The Coconut Programming Language, Copyright 2015-2016 Evan Hubinger.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use these files except in compliance with the License.
+You may obtain a copy of the License at:
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 #-----------------------------------------------------------------------------------------------------------------------
