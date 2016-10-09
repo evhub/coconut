1283dca05fa6d2b820a0b02d8c939dffac1be179
Evan Hubinger <evanjhub@gmail.com>
2016-10-08 23:09:20 -0700

    Updates develop version

--- coconut/root.py
+++ coconut/root.py
@@ -25,7 +25,7 @@ import sys as _coconut_sys
 
 VERSION = "1.2.0"
 VERSION_NAME = "Colonel"
-DEVELOP = 5
+DEVELOP = 6
 
 #-----------------------------------------------------------------------------------------------------------------------
 # CONSTANTS:
