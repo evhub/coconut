159a0298a2692ff057bb5788abe25c2aa5ebfb16
Evan Hubinger <evanjhub@gmail.com>
2017-01-22 15:21:05 -0800

    Fixes an incorrect import

--- coconut/__init__.py
+++ coconut/__init__.py
@@ -47,7 +47,7 @@ def load_ipython_extension(ipython):
 
     # add magic function
     from coconut.convenience import cmd, parse, CoconutException
-    from coconut.logging import logger
+    from coconut.display import logger
 
     def magic(line, cell=None):
         """Provides %coconut and %%coconut magics."""
