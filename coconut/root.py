308339f1fa7cbaee06be1ed0175926e5fb8ec3c8
Evan Hubinger <evanjhub@gmail.com>
2017-03-21 20:46:32 -0700

    Universalizes object
    Refs #233.

--- coconut/root.py
+++ coconut/root.py
@@ -42,13 +42,22 @@ VERSION_STR_TAG = "v" + VERSION_STR
 PY2 = _coconut_sys.version_info < (3,)
 PY26 = _coconut_sys.version_info < (2, 7)
 
-PY3_HEADER = r'''py_chr, py_filter, py_hex, py_input, py_int, py_map, py_oct, py_open, py_print, py_range, py_str, py_zip, py_filter, py_reversed, py_enumerate = chr, filter, hex, input, int, map, oct, open, print, range, str, zip, filter, reversed, enumerate
+PY3_HEADER = r'''py_chr, py_filter, py_hex, py_input, py_int, py_map, py_object, py_oct, py_open, py_print, py_range, py_str, py_zip, py_filter, py_reversed, py_enumerate = chr, filter, hex, input, int, map, object, oct, open, print, range, str, zip, filter, reversed, enumerate
 '''
 PY27_HEADER = PY3_HEADER + r'''py_raw_input, py_xrange = raw_input, xrange
-_coconut_raw_input, _coconut_xrange, _coconut_int, _coconut_long, _coconut_print, _coconut_str, _coconut_unicode, _coconut_repr = raw_input, xrange, int, long, print, str, unicode, repr
+_coconut_NotImplemented, _coconut_raw_input, _coconut_xrange, _coconut_int, _coconut_long, _coconut_print, _coconut_str, _coconut_unicode, _coconut_repr = NotImplemented, raw_input, xrange, int, long, print, str, unicode, repr
 from future_builtins import *
 chr, str = unichr, unicode
 from io import open
+class object(object):
+    if hasattr(object, "__doc__"):
+        __doc__ = object.__doc__
+    def __ne__(self, other):
+        eq = self == other
+        if eq is _coconut_NotImplemented:
+            return eq
+        else:
+            return not eq
 class range(object):
     __slots__ = ("_xrange",)
     if hasattr(_coconut_xrange, "__doc__"):
