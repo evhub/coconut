693b0d0ecb25d2484928a1f1ac796735a891d075
Evan Hubinger <evanjhub@gmail.com>
2017-10-22 13:45:02 -0700

    Fix jupyter test

--- coconut/command/util.py
+++ coconut/command/util.py
@@ -215,13 +215,12 @@ def run_file(path):
         return runpy.run_path(path, run_name="__main__")
 
 
-def call_output(cmd, stdin=(), **kwargs):
+def call_output(cmd, **kwargs):
     """Run command and read output."""
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
     stdout, stderr, retcode = [], [], None
-    stdin_list = list(reversed(stdin))
     while retcode is None:
-        out, err = p.communicate(stdin_list.pop() if stdin_list else None)
+        out, err = p.communicate()
         if out is not None:
             stdout.append(out.decode(get_encoding(sys.stdout)))
         if err is not None:
