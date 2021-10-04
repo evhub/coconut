e04889921137a6c0c632d4c7d4939edc87952ffe
Evan Hubinger <evanjhub@gmail.com>
2021-10-04 01:21:10 -0700

    Fix prelude test

--- tests/main_test.py
+++ tests/main_test.py
@@ -422,7 +422,7 @@ def comp_prelude(args=[], **kwargs):
 
 def run_prelude(**kwargs):
     """Runs coconut-prelude."""
-    call(["pip", "install", "-e", prelude])
+    call(["make", "base-install"])
     call(["pytest", "--strict", "-s", os.path.join(prelude, "prelude")], assert_output="passed", **kwargs)
 
 
